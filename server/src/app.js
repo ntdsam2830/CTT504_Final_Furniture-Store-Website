3511c61680af093e077f7d0c9f4157cbcf309533
qhuongng <nq.huong.2304@gmail.com>
2024-04-29 22:42:59 +0700

    Fix CORS error

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const {errorResposerHandler, invalidPathHandler} = require('./middleware/errorHandler');
+const { errorResposerHandler, invalidPathHandler } = require('./middleware/errorHandler');
 require('express-async-errors');
+require('dotenv').config();
 
 //-----------------------------------
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOption = {
-    origin: "http://localhost:3000",
+    origin: [process.env.SELLER_URL, process.env.USER_URL],
     credentials: true
 }
 app.use(cors(corsOption));// sau này chỉnh lại thành đg dẫn mặc định
@@ -21,10 +22,10 @@ const cartRoute = require('./routes/cartRouter');
 
 //-----------------------------------
 
-app.use('/api/product',productRoute);
-app.use('/api/user',userRoute);
+app.use('/api/product', productRoute);
+app.use('/api/user', userRoute);
 app.use('/api/cart', cartRoute);
-app.use('/api/picture',express.static('public'));
+app.use('/api/picture', express.static('public'));
 
 app.use(invalidPathHandler);
 app.use(errorResposerHandler);
