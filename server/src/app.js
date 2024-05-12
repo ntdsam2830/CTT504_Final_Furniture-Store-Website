df656ac0191e45e59735731301678a34413fe33a
qhuongng <nq.huong.2304@gmail.com>
2024-05-12 13:45:54 +0700

    Modify CORS option

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOption = {
-  origin: [process.env.SELLER_URL, process.env.USER_URL],
+  origin: '*',
   methods: '*',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
