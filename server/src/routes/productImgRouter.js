3fc6457a24ff34e420ec23573e8f6a811d44da5b
qhuongng <nq.huong.2304@gmail.com>
2024-05-12 18:48:48 +0700

    Reorder routes

--- server/src/routes/productImgRouter.js
+++ server/src/routes/productImgRouter.js
@@ -6,7 +6,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 const middleware = require('../middleware/authMiddleware')
 
+router.post('/saveImgProduct', upload.array('image', 4), productImgController.saveProductImg);
 router.get('/getImgProduct/:id', productImgController.getProductImg);
-router.post('/saveImgProduct', upload.array('image', 4), productImgController.saveProductImg)
 
 module.exports = router
--- server/src/routes/reviewRouter.js
+++ server/src/routes/reviewRouter.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const middleware = require('../middleware/authMiddleware')
 
 router.post('/addReview', middleware.authGuard, reviewController.addReview);
-router.get('/getReview/:id', reviewController.getReview);
 router.put('/updateListFavor', middleware.authGuard, reviewController.updateFavorites);
+router.get('/getReview/:id', reviewController.getReview);
 
 module.exports = router;
