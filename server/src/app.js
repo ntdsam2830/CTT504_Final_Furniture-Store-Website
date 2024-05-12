867ab4210f46201a58b272dd0fac9e5590480dbe
qhuongng <nq.huong.2304@gmail.com>
2024-05-12 16:01:02 +0700

    Fix CORS

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOption = {
-  origin: true,
+  origin: "*",
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
